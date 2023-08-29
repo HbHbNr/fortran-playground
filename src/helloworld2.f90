f18a1b5b432cb2052f0366acb0bb4bba95b6992c
Sebastian <hbhbnr@posteo.net>
2023-08-30 01:24:18 +0200

    Extend Fortran playground

--- src/helloworld2.f90
+++ src/helloworld2.f90
@@ -1,6 +1,28 @@
+integer function mytoint(string)
+    implicit none
+
+    character(20) :: string
+    read(string, '(I20)') mytoint
+    ! integer :: result
+    ! result = 666
+    ! mytoint = result
+end function mytoint
+
 program helloworld2
     implicit none
-    character(80) :: message
+
+    character(20) :: message
+    integer :: length
+    character(20) :: caloriesstring
+    integer :: calories
+    integer :: mytoint
+
     message = 'Hello, world!'
-    print '(Aa)',message
+    length = len(message)
+    print '(Aa)',message,message
+    print '(I4)',length
+
+    caloriesstring = '2000'
+    calories = mytoint(caloriesstring)
+    print '(I4)',calories+1
 end program helloworld2
