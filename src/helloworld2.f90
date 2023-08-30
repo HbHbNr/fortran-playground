4b94d3e1f05565f05e60a17caf11176f3133798e
Sebastian <hbhbnr@posteo.net>
2023-08-30 18:34:33 +0200

    Extend Fortran playground

--- src/helloworld2.f90
+++ src/helloworld2.f90
@@ -1,28 +1,33 @@
-integer function mytoint(string)
-    implicit none
-
-    character(20) :: string
-    read(string, '(I20)') mytoint
-    ! integer :: result
-    ! result = 666
-    ! mytoint = result
-end function mytoint
-
-program helloworld2
+subroutine sayhello
     implicit none
 
     character(20) :: message
     integer :: length
-    character(20) :: caloriesstring
-    integer :: calories
-    integer :: mytoint
 
     message = 'Hello, world!'
     length = len(message)
     print '(Aa)',message,message
     print '(I4)',length
+end subroutine
+
+integer function strtoint(string)
+    implicit none
+
+    character(20) :: string
+
+    read(string, '(I20)') strtoint  ! return mytoint
+end function strtoint
+
+program helloworld2
+    implicit none
+
+    character(20) :: caloriesstring
+    integer :: calories
+    integer :: strtoint
+
+    call sayhello
 
     caloriesstring = '2000'
-    calories = mytoint(caloriesstring)
+    calories = strtoint(caloriesstring)
     print '(I4)',calories+1
 end program helloworld2
