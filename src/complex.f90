10c5e161f8adbdfc124dddbb0785fc610ab6a7e2
Sebastian <hbhbnr@posteo.net>
2023-10-03 23:36:10 +0200

    Add tests for complex numbers

--- src/complex.f90
+++ src/complex.f90
@@ -6,9 +6,14 @@ program complex
     complex(kind=4)  :: c2 = (1,2)
     complex(kind=8)  :: c3 = (1,2)
     complex(kind=16) :: c4 = (1,2)
+    integer          :: r, i
 
     print *, c1, (c1 / 2)
     print *, c2, (c2 / 2)
     print *, c3, (c3 / 2)
     print *, c4, (c4 / 2)
+    r = 3
+    i = 5
+    c1 = cmplx(r, i)
+    print *, c1, (c4 / 2)
 end program complex
